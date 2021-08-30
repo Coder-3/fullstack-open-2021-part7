49f79192e637186e73806964088c204c8cd67330
Cedric Quenette <cedricquenette@outlook.com>
2021-08-31 07:06:16 +1000

    7.5: anecdotes and hooks step2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -86,6 +86,14 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const handleReset = event => {
+    event.preventDefault()
+
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -103,6 +111,7 @@ const CreateNew = (props) => {
           <input  {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
